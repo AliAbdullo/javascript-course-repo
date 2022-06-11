6b141d3aa8c2d2f5effda59582164f0273354547
AliAbdulloh <74522999+AliAbdullo@users.noreply.github.com>
2022-06-11 13:44:02 +0500

    Update Algoritm.js

--- Algoritm.js
+++ Algoritm.js
@@ -9,3 +9,25 @@ const bar = (n) => {
   bar(n - 2);
 };
 
+const dib = (n) => {
+  if (n <= 1) return;
+  dib(n -1);
+  dib(n- 1);
+};
+
+
+const canSum = (targetSum,numbers) => {
+  if (targetSum === 0 ) return true;
+  if (targetSum < 0) return false;
+  
+  for (let num of numbers) {
+    const reminder = targetSum - num;
+    if (canSum(remainder, numbers) ===true) {
+      return true;
+    }
+  }
+  return false;
+};
+
+
+console.log(canSum(7,[2,3])); //true
